5de5283f2563d7213750601014be753286b9bcb7
Noah Sussman <ns@noahsussman.com>
2012-10-02 12:12:10 -0400

    work on graphing a newline-delimited list of integer values followed by labels, eg:
     99 foo bar
     55 baz boz
      9 homer marge
      1 bart maggie

--- graphs.py
+++ graphs.py
@@ -10,18 +10,26 @@
 # Currently usage is like:
 #
 #     git log --since='september' --format='%ad' --date-order | \
-#        awk '{print $1" "$2" "$3}' | uniq -c | tac | \
-#        awk '{print $1}' | \
-#        xargs python ~/git/Arcane/snippets/python/graphs.py
+    #        awk '{print $1" "$2" "$3}' | uniq -c | tac | \
+    #        awk '{print $1}' | \
+    #        xargs python ~/git/Arcane/snippets/python/graphs.py
 
 import matplotlib.pyplot as plt
 import sys
 
 sys.argv.pop(0)
 
-args = [float(s) for s in sys.argv]
+raw_values = sys.argv
+values = []
 
-plt.plot(args)
-plt.xticks(range(len(sys.argv)), ['foo'], size='small', rotation='vertical')
+for s in raw_values:
+    tokens = s.split()
+    cons = tokens.pop(0)
+    cdr = tokens
+    values.append(float(cons))
 
+# values = [float(s) for s in raw_values]
+
+plt.plot(values)
+plt.xticks(range(len(values)), ['foo'], size='small', rotation='vertical')
 plt.show()
