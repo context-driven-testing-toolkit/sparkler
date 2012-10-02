0fd84d231c26211fda57055df4856bc68674b397
Noah Sussman <ns@noahsussman.com>
2012-10-02 14:53:57 -0400

    correctly handle all three valid input conditions:
    1. space-delimited list of values
    2. newline-delimited list of values
    3. newline-delimited list of values with labels

--- graphs.py
+++ graphs.py
@@ -7,29 +7,32 @@
 # see http://matplotlib.org/users/pyplot_tutorial.html
 #     http://stackoverflow.com/questions/1992640
 #
-# Currently usage is like:
-#
-#     git log --since='september' --format='%ad' --date-order | \
-#            awk '{print $1" "$2" "$3}' | uniq -c | tac | \
-#            awk '{print $1}' | \
-#            xargs python ~/git/Arcane/snippets/python/graphs.py
-#
-# Or if you want labels on the y axis you can do:
-# 
-#     git log --since 'september' --format='%ad' --date-order | \
-#            awk '{print $1" "$2" "$3}' | uniq -c | tac | \
-#            xargs -0 python ~/git/Arcane/snippets/python/graphs.py
-#
 # There are three use cases in all:
-#     1. space-delimited list of values
-#     2. newline-delimited list of values
-#     3. newline delimited list where each line is a value followed by label text
+# 1. space-delimited list of values (no labels, all values on one line):
+#
+#     git log --since 'september' --format='%ad' --date-order | \
+#         awk '{print $1" "$2" "$3}' | uniq -c | tac | awk '{print $1}' | \
+#         xargs python graphs.py
+#
+# 2. newline-delimited list of values (no labels, newline-delimited):
+#
+#      git log --since 'september' --format='%ad' --date-order | \
+#          awk '{print $1" "$2" "$3}' | uniq -c | tac | awk '{print $1}' | \
+#          python graphs.py
+# 
+# 3. newline-delimited list where each line is a value followed by label text:
+#
+#     git log --since 'september' --format='%ad' --date-order | \
+#         awk '{print $1" "$2" "$3}' | uniq -c | tac | \
+#         python graphs.py
 
-import matplotlib.pyplot as plt
 import sys
+import matplotlib.pyplot as plt
 
-if len(sys.argv) == 2 and sys.argv[1].match('\n'):
-    raw_values = sys.argv[1].strip().split('\n')
+lines_of_input = sys.stdin.readlines()
+
+if len(sys.argv) == 1 and len(lines_of_input) > 0:
+    raw_values = lines_of_input
 
     values = []
     labels = []
