d4c956e787f685ed123725e5df24b6690507c14b
Noah Sussman <ns@noahsussman.com>
2012-10-02 13:02:47 -0400

    crap! this whole time I should have been using
        print sys.stdin.readlines()
    
    instead of sys.argv
    
    the former handles multiline argument lists...
    
    >.<

--- graphs.py
+++ graphs.py
@@ -13,11 +13,22 @@
 #            awk '{print $1" "$2" "$3}' | uniq -c | tac | \
 #            awk '{print $1}' | \
 #            xargs python ~/git/Arcane/snippets/python/graphs.py
+#
+# Or if you want labels on the y axis you can do:
+# 
+#     git log --since 'september' --format='%ad' --date-order | \
+#            awk '{print $1" "$2" "$3}' | uniq -c | tac | \
+#            xargs -0 python ~/git/Arcane/snippets/python/graphs.py
+#
+# There are three use cases in all:
+#     1. space-delimited list of values
+#     2. newline-delimited list of values
+#     3. newline delimited list where each line is a value followed by label text
 
 import matplotlib.pyplot as plt
 import sys
 
-if len(sys.argv) == 2:
+if len(sys.argv) == 2 and sys.argv[1].match('\n'):
     raw_values = sys.argv[1].strip().split('\n')
 
     values = []
@@ -31,6 +42,11 @@ if len(sys.argv) == 2:
         labels.append(cdr)
         plt.plot(values)
         plt.xticks(range(len(values)), labels, size='small', rotation='vertical')
+
+elif len(sys.argv) == 2:
+    raw_values = sys.argv[1].split('\n')
+    values = [float(s) for s in raw_values]
+    plt.plot(values)
 
 else:
     sys.argv.pop(0)
