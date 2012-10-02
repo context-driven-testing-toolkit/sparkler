f07b6f72cbd0f3f115f2357ccab96ca1f2458023
Noah Sussman <ns@noahsussman.com>
2012-10-02 09:24:38 -0400

    A cli tool like spark, that produces sparklines from whitespace-delimited lists of numbers passed by other command line arguments.

---
+++ graphs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# A cli tool like spark, that produces sparklines from
+# whitespace-delimited lists of numbers passed by other command line
+# arguments.
+#
+# see http://matplotlib.org/users/pyplot_tutorial.html
+#
+# Currently usage is like:
+#
+#     git log --since='september' --format='%ad' --date-order | \
+#        awk '{print $1" "$2" "$3}' | uniq -c | tac | \
+#        awk '{print $1}' | \
+#        xargs python ~/git/Arcane/snippets/python/graphs.py
+
+import matplotlib.pyplot as plt
+import sys
+
+sys.argv.pop(0)
+
+args = [float(s) for s in sys.argv]
+
+plt.plot(args)
+plt.show()
