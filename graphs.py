1cc4fe2915f81addf5a527ddbba2329784f38e05
Noah Sussman <ns@noahsussman.com>
2012-10-03 00:20:01 -0400

    correctly handle command line arguments in addition to piped input

--- graphs.py
+++ graphs.py
@@ -35,40 +35,45 @@ import matplotlib.pyplot as plt
 def make_plot(values):
     plt.plot(values, color='#333333', linewidth=2.0)
 
-if len(sys.argv) > 1:
-    sys.argv.pop(0)
-    raw_values = sys.argv
+def plot_integers(raw_values):
     values = [float(s) for s in raw_values]
     make_plot(values)
-else:
-    
-    lines_of_input = sys.stdin.readlines()
 
-    if len(sys.argv) == 1 and len(lines_of_input) > 1:
-        raw_values = lines_of_input
-
-        values = []
-        labels = []
-
-        for s in raw_values:
-            tokens = s.split()
-            cons = tokens.pop(0)
-            cdr = ' '.join(tokens)
-            values.append(float(cons))
-            labels.append(cdr)
-            make_plot(values)
-            plt.xticks(range(len(values)), labels, size='small', rotation=75)
-
-    elif len(sys.argv) == 1 and len(lines_of_input) == 1:
-        raw_values = lines_of_input[0].split(' ')
-        values = [float(s) for s in raw_values]
-        make_plot(values)
-
-    else:
+def get_inputs():
+    if len(sys.argv) > 1:
         sys.argv.pop(0)
-        raw_values = sys.argv
-        values = [float(s) for s in raw_values]
+
+        if len(sys.argv) == 1:
+            return sys.argv[0].strip().split('\n')
+        else:
+            return sys.argv
+    else:
+        raw_input = sys.stdin.readlines()
+        if len(raw_input) == 1:
+            return raw_input[0].strip().split(' ')
+        else:
+            return raw_input
+
+raw_values = get_inputs()
+
+if len(raw_values) > 1 and len(raw_values[0].strip().split(' ')) > 1:
+    values = []
+    labels = []
+
+    for s in raw_values:
+        tokens = s.strip().split()
+        cons = tokens.pop(0)
+        cdr = ' '.join(tokens)
+        values.append(float(cons))
+        labels.append(cdr)
         make_plot(values)
+        plt.xticks(range(len(values)), labels, size='small', rotation=75)
+
+elif len(raw_values) > 1:
+    plot_integers(raw_values)
+
+else:
+     plot_integers(raw_values[0].strip().split(' '))
 
 fig = matplotlib.pyplot.gcf()
 fig.set_size_inches(14, 8.65)
