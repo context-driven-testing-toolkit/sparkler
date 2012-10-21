1b07bcc7261133384d752bd831be2d98aea881dd
Noah Sussman <ns@noahsussman.com>
2012-10-21 13:44:08 -0400

    rename plot image file used in test

--- tests/lib/line_plot_above_100_data_points_test.sh
+++ tests/lib/line_plot_above_100_data_points_test.sh
@@ -6,4 +6,4 @@ test -e $actual && rm $actual
 
 ruby -e 'x=1; 101.times {puts (x *= 1.02).to_s + " lorem ipsum"}' | \
     bin/sparkler > /dev/null && \
-    diff tests/plots/scatterplot_above_100_data_points.png $actual
+    diff tests/plots/more_than_100_data_points.png $actual
